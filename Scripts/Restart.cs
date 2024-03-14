c58b76f4f06f21eb4bdc51bec97a96961eb588e0
Aswinsnath <115206561+Aswinsnath@users.noreply.github.com>
2024-03-14 13:21:34 +0530

    Add files via upload

---
+++ Scripts/Restart.cs
@@ -0,0 +1,19 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Restart : MonoBehaviour
+{
+    public GameObject uiElement;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            uiElement.SetActive(true);
+        }
+    }
+}
+*/
